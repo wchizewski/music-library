ac30e21e8caaef6a5e821d62bcd3f7fbc2ce7ea7
wchizewski <90284175+wchizewski@users.noreply.github.com>
2022-10-14 14:47:47 -0600

    begun

---
+++ main.js
@@ -0,0 +1,10 @@
+// Music Library
+
+// elements
+let menu1El = document.getElementById("menu1");
+let menu2El = document.getElementById("menu2");
+let outputEl = document.getElementById("output");
+let searchBarEl = document.getElementById("search-bar");
+
+// Array
+let music = [];
