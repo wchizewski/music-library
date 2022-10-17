91644dc8ab0de19e01fec93587ea1d0fb817bf61
wchizewski <90284175+wchizewski@users.noreply.github.com>
2022-10-17 14:48:39 -0600

    monday

--- main.js
+++ main.js
@@ -4,7 +4,29 @@
 let menu1El = document.getElementById("menu1");
 let menu2El = document.getElementById("menu2");
 let outputEl = document.getElementById("output");
+let searchMenuEl = document.getElementById("search-menu");
 let searchBarEl = document.getElementById("search-bar");
+let selection = searchMenuEl.value;
 
 // Array
-let music = [];
+
+// search
+// function search() {
+//   if (selection === "album") {
+//     searchAlbums();
+//   } else if (selection === "song") {
+//     searchSongs();
+//   }
+// }
+
+searchBarEl.addEventListener("keydown", searchBarHandler);
+
+function searchBarHandler(e) {
+  if (e.code === "Enter" && selection === "album") {
+    searchAlbums();
+  } else if (e.code === "Enter" && selection === "song") {
+    searchSongs();
+  }
+  //   let query = searchBarEl.value;
+  //   searchBarEl.value = "";
+}
