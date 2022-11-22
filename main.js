c71c11981053b122d3cb503c79a8b69dd0cd70d5
wchizewski <90284175+wchizewski@users.noreply.github.com>
2022-11-22 14:48:16 -0700

    tuesday!

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ let library = [];
 
 // Token
 let token =
-  "BQBtFUsvq10CZCBzHZmgdUeBvuVDba004rF4DGCwTAQk1t3q5xMtrQqcKy4YecDUAOqbMkHM4Tlr1mgH8q4WTyD3ZYp1VGe7M0bbJAA5wlhU60H3HUhMDab9ffzP8H6y0S1kSN1TcmDUZ4uipJKT3iUSzWoKScXFVf42ISVt8kP0NQMwpruCLImEuTmevYDNUI6aMaVrVO6twN8l";
+  "BQAPp5cCEvmwq9AKQ-R5MVv4V1JWiGJv8FfjCjEjhG5b1ivCImgR5QzY_7cmvS__JNrfmosLtlJCZGB-LJCv5nYbnZTADFvN3QS-_pLUHHXXRZf1bD-iMJqaHLMq6VgruFZjbq6rPrefPlD-XesyhXrRouuBA2xCp9LtUqcTbbvDBF6_PfBpfkypiI5F-G-d8sg7rHyIvTHx-kKh";
 let albumNum = 300;
 processData();
 
@@ -38,29 +38,6 @@ function processData() {
       });
   }
 }
-
-// // genres
-// let genres = [];
-// // Token
-// let token2 =
-//   "BQDkjEvB8unqtSs1JGjtDt9iYRBWeXfXxGJCTULxZPgGVMyZr9x5aL2xU7lCKsZ5uQ61sPvvYxCEb_i7dlLs9T3yq7li3DQpQ1K7E5VozvTETqU_Knwhi07QcifnYSnZAh3CfpfYe81FjWpv_KG8QdcUpKFx6_uVNyogu8nAk9BpVKmBQI7C5UzfuX-sUfy8-IONYMQ-F2ia60eR";
-// processData2();
-
-// function processData2() {
-//   let totalData = [];
-//   fetch(`https://api.spotify.com/v1/recommendations/available-genre-seeds`, {
-//     headers: new Headers({
-//       Authorization: `Bearer ${token2}`,
-//       ContentType: `application/json`,
-//     }),
-//   })
-//     .then((response) => response.json())
-//     .then((data) => {
-//       totalData.push(data.items);
-//       let genre = totalData.flat();
-//       genres = genre;
-//     });
-// }
 
 // Sorting Albums
 document.getElementById("menu2-btn").addEventListener("click", goBtn2Handler);
@@ -159,6 +136,19 @@ function getArtistStr(album) {
   return artistStr;
 }
 
+function getSongArtistsStr(trackItems) {
+  let songArtists = trackItems.artists;
+  let songArtistsStr = "";
+  songArtists.forEach((songArtists, index) => {
+    if (index + 1 == songArtists.length) {
+      songArtistsStr += songArtists.name;
+    } else {
+      songArtistsStr += songArtists.name + ", ";
+    }
+  });
+  return songArtistsStr;
+}
+
 function getAlbumDiv(album, artistStr, i) {
   // img
   let imgEl = document.createElement("img");
@@ -193,25 +183,8 @@ function openAlbum(e) {
   let trackItems = library[albumIndex].album.tracks.items;
   let album = library[albumIndex].album;
   outputEl.innerHTML = "";
-  let artists = library[albumIndex].album.artists;
-  let artistStr = "";
-  artists.forEach((artist, index) => {
-    if (index + 1 == artists.length) {
-      artistStr += artist.name;
-    } else {
-      artistStr += artist.name + ", ";
-    }
-  });
-
-  albumOutputEl.innerHTML = `
-    <div>
-      <p>
-      <img src="${album.images[1].url}">
-      <h1>${album.name}</h1>
-      <h3>${artistStr} • ${album.release_date}</h3>
-      </p>
-    </div>
-    `;
+  let artistStr = getArtistStr(album);
+  albumOutputEl.appendChild(openAlbumDiv(album, artistStr));
   for (let i = 0; i < trackItems.length; i++) {
     let songArtists = trackItems[i].artists;
     let songArtistsStr = "";
@@ -222,40 +195,35 @@ function openAlbum(e) {
         songArtistsStr += songArtists.name + ", ";
       }
     });
+    // let songArtistsStr = getSongArtistsStr(trackItems);
     let min = Math.floor(trackItems[i].duration_ms / 60000);
     let sec = Math.floor((trackItems[i].duration_ms % 60000) / 1000);
     if (sec.toString().length === 1) {
       sec = "0" + sec;
     }
     let duration = min + ":" + sec;
-    // outputEl.appendChild(
-    //   getAlbumDiv(album, artistStr, i, songArtistsStr, trackItems, duration)
-    // );
-    albumOutputEl.innerHTML += `
-    <p id="tracks" data-id="${i}">
-    ${trackItems[i].name}
-    <span>
-    ${duration}
-    </span>
-    <input type="checkbox" id="like-btn${i}" data-id="${i}">
-    <br>
-    ${songArtistsStr}
-    </p>
-    `;
+    albumOutputEl.appendChild(
+      getTracklistDiv(i, songArtistsStr, trackItems, duration)
+    );
+    // albumOutputEl.innerHTML += `
+    // <p id="tracks" data-id="${i}">
+    // ${trackItems[i].name}
+    // <span>
+    // ${duration}
+    // </span>
+    // <input type="checkbox" id="like-btn${i}" data-id="${i}">
+    // <br>
+    // ${songArtistsStr}
+    // </p>
+    // `;
   }
   // for (let i = 0; i < trackItems.length; i++) {
   //   document.getElementById(`like-btn${i}`).addEventListener("input", likeSong);
   // }
 }
 
-function getTracklistDiv(
-  album,
-  artistStr,
-  i,
-  songArtistsStr,
-  trackItems,
-  duration
-) {
+// Open Album Function: Cover + Name + Artist(s) + Date
+function openAlbumDiv(album, artistStr) {
   // img
   let imgEl = document.createElement("img");
   imgEl.src = album.images[1].url;
@@ -266,26 +234,34 @@ function getTracklistDiv(
 
   // h3
   let h3El = document.createElement("h3");
-  h3El.innerHTML = artistStr + "•" + album.release_date;
+  h3El.innerHTML = artistStr + " • " + album.release_date;
 
+  let divEl = document.createElement("div");
+  divEl.appendChild(imgEl);
+  divEl.appendChild(h1El);
+  divEl.appendChild(h3El);
+
+  return divEl;
+}
+
+// Open Album Function: Tracklist + Like Btn
+function getTracklistDiv(i, songArtistsStr, trackItems, duration) {
   // p (tracklist)
   let tracklistEl = document.createElement("p");
-  tracklistEl.innerHTML = trackItems[i].name + duration + songArtistsStr;
+  tracklistEl.id = "tracks";
+  tracklistEl.innerHTML =
+    trackItems[i].name + " " + duration + "<br>" + songArtistsStr;
 
   // like btn
-  let checkboxEl = document.createElement("input");
-  checkboxEl.type = "checkbox";
-  checkboxEl.dataset.index = index;
-  checkboxEl.checked = task.completed;
-  checkboxEl.addEventListener("input", likeSong);
+  // let checkboxEl = document.createElement("input");
+  // checkboxEl.type = "checkbox";
+  // checkboxEl.dataset.index = i;
+  // checkboxEl.checked = likebtn.completed;
+  // checkboxEl.addEventListener("input", likeSong);
 
   // div
   let divEl = document.createElement("div");
   divEl.dataset.index = i;
-  divEl.addEventListener("click", openAlbum);
-  divEl.appendChild(imgEl);
-  divEl.appendChild(h1El);
-  divEl.appendChild(h3El);
   divEl.appendChild(tracklistEl);
 
   return divEl;
